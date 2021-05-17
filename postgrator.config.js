6a0915bbf909470570fd4a79c81e9b5216b78eb4
Kris Kettendorf <triggxl@hotmail.com>
2021-05-17 14:44:29 -0400

    installed postgrator

---
+++ postgrator.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "migrationDirectory": "migrations",
+  "driver": "pg",
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+}
